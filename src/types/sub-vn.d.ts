308b00b799fa9c3e376f7f038876bb2d6e8c346f
Wint05 <thanglvph48864@gmail.com>
2025-11-04 16:57:33 +0700

    feat: add sub-vn module and update package dependencies
    - Introduced the 'sub-vn' module with TypeScript definitions for provinces, districts, and wards.
    - Updated package.json and package-lock.json to include new dependencies and versions, enhancing project functionality and compatibility.

--- src/types/sub-vn.d.ts
+++ src/types/sub-vn.d.ts
@@ -1,1 +1,28 @@
+declare module 'sub-vn/json_data/provinces.json' {
+  interface Province {
+    code: string;
+    name: string;
+  }
+  const provinces: Province[];
+  export default provinces;
+}
 
+declare module 'sub-vn/json_data/districts.json' {
+  interface District {
+    code: string;
+    name: string;
+    province_code: string;
+  }
+  const districts: District[];
+  export default districts;
+}
+
+declare module 'sub-vn/json_data/wards.json' {
+  interface Ward {
+    code: string;
+    name: string;
+    district_code: string;
+  }
+  const wards: Ward[];
+  export default wards;
+}
